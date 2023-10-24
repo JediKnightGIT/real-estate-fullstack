31d7837859639cbf0fba3fd5e0372837437d074e
Jedi Knight <jediknightru@gmail.com>
2023-10-24 13:35:10 +0300

    connected to database

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,20 @@
-import express, { Application } from 'express'
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app: Application = express()
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO || '')
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app: Application = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-}) 
+  console.log('Server is running on port 3000');
+});
