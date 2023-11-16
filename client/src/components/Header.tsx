d6bd24569fab4f89e69ce662d22ba1359f284147
JediKnightGIT <jediknightru@gmail.com>
2023-11-16 13:49:36 +0300

    completed header search feature

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,31 @@
+import React from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useAppSelector } from '../redux/hooks';
 import { RootState } from '../redux/store';
 
 const Header: React.FC = () => {
   const { currentUser } = useAppSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = React.useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  React.useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, []);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,13 +36,17 @@ const Header: React.FC = () => {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
 
         <ul className="flex gap-4">
@@ -34,7 +58,11 @@ const Header: React.FC = () => {
           </Link>
           <Link to="/profile">
             {currentUser ? (
-              <img src={currentUser.avatar} alt={currentUser.username + '\'s avatar'} className='rounded-full w-7 h-7 object-cover' />
+              <img
+                src={currentUser.avatar}
+                alt={currentUser.username + "'s avatar"}
+                className="rounded-full w-7 h-7 object-cover"
+              />
             ) : (
               <li className="hidden sm:inline text-slate-700 hover:underline">Sign in</li>
             )}
