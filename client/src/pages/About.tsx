5445d500d3d4c59fba6d16bd09387cc4907c615d
Jedi Knight <jediknightru@gmail.com>
2023-11-19 12:27:53 +0300

    completed about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div>About</div>
+    <>
+      <div>About</div>
+      <div className="py-20 px-4 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-slate-800">About Real Estate</h1>
+        <p className="mb-4 text-slate-700">
+          Real Estate is a leading real estate agency specializing in assisting clients with buying,
+          selling, and renting properties in highly sought-after neighborhoods. Our team of
+          experienced agents is dedicated to providing exceptional service and ensuring a smooth
+          buying and selling process.
+        </p>
+        <p className="mb-4 text-slate-700">
+          Our mission is to help clients achieve their real estate goals by offering expert advice,
+          personalized service, and a deep understanding of the local market. Whether you're
+          interested in buying, selling, or renting a property, we're here to assist you every step
+          of the way.
+        </p>
+        <p className="mb-4 text-slate-700">
+          With a wealth of experience and knowledge in the real estate industry, our team of agents
+          is committed to delivering the highest level of service to our clients. We believe that
+          buying or selling a property should be an exciting and rewarding experience, and we're
+          dedicated to making that a reality for each and every client.
+        </p>
+      </div>
+    </>
   );
 };
 
